229617ae89bad9a5db9e031bbf5a7590b0be2673
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-07-04 07:41:33 +0700

    On progress styling

--- app/countries/[id]/page.js
+++ app/countries/[id]/page.js
@@ -9,6 +9,7 @@ import Image from "next/image";
 const Country = ({ params }) => {
     const { id } = params;
     const [CountriesByID, setCountriesByID] = useState(null);
+    const [bordersNeighbour, setBordersNeighbour] = useState([])
 
     useEffect(() => {
         const getCountryByName = async () => {
@@ -18,25 +19,37 @@ const Country = ({ params }) => {
           setCountriesByID(countries);
         };
         getCountryByName();
+        
+        const { borders } = countries[0];
+        setBordersNeighbour(
+          // next works in here!!!
+        )
       }, [id])
 
     if (CountriesByID) {
       //  destruct the object in here
-      const { name, flags, region, population, area } = CountriesByID[0];
+      const { name, flags, region, population, area, capital, languages, currencies } = CountriesByID[0];
+      const { nativeName, common } = name;
 
-      return <Layout title={CountriesByID[0].name.common}>
+      const fullLanguage = Object.keys(languages).map((key) => languages[key]).join(", ")
+      const fullCurrencies = Object.keys(currencies).map((key) => currencies[key].name).join(", ")
+      const fullNativeNames = Object.keys(nativeName).map((key) => nativeName[key].common).join(", ")
+
+      // console.log(fullNativeNames)
+
+      return <Layout title={name.common}>
         <div>
           <div className={styles.overview_panel}>
             <div className={styles.overview_image}>
               <Image 
                 src={flags.svg}
-                alt={name.common}
+                alt={common}
                 fill={true}
                 priority={true}
               />
             </div>
 
-            <h1 className={styles.overview_name}>{name.common}</h1>
+            <h1 className={styles.overview_name}>{common}</h1>
             <div className={styles.overview_region}>{region}</div>
 
             <div className={styles.overview_numbers}>
@@ -49,6 +62,30 @@ const Country = ({ params }) => {
                 <div className={styles.overview_value}>{area}</div>
                 <div className={styles.overview_label}>Area</div>
               </div>
+            </div>
+          </div>
+
+          <div className={styles.details_panel}>
+            <h4 className={styles.details_panel_heading}>Details</h4>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Capital</div>
+              <div className={styles.details_panel_value}>{capital}</div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Language</div>
+              <div className={styles.details_panel_value}>{fullLanguage}</div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Currencies</div>
+              <div className={styles.details_panel_value}>{fullCurrencies}</div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Native name</div>
+              <div className={styles.details_panel_value}>{fullNativeNames}</div>
             </div>
           </div>
         </div>
